895252965cfc28ca516c65aaafcca50e485cb7d0
jiamzhang <jiamzhang@ebay.com>
2015-10-11 23:05:27 +0800

    abc

--- src/cn/lynx/emi/license/GenerateLicense.java
+++ src/cn/lynx/emi/license/GenerateLicense.java
@@ -1,25 +1,20 @@
 package cn.lynx.emi.license;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
 
 import org.apache.commons.codec.binary.Base64;
 
 public class GenerateLicense {
-	private static final String LICENSE_CORE_KEY = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAoc7HGhetqiAwyrVZxBskNusiz9TNPX0niaIi7C16DxnKguANpn1lDOk/U2T+gOJiLIt/zL3bvhjCXN0krD4lZUJZxC2RXSQG97622aFeYuYOKtkzmrNwlRK8RCeKlGbydF9V7O+LmKHEzlpttLx0pglw0x4ps4ALEc82wZErHhZ9m76m1ykoNOSY+Khz4OPhMVXKm0EYwitTktfSEsV/vIsXymbJCUprkN1Nw7ftjA3UyU9LvRhs1puczss8kp0WLE9gOB9dxzV+QrmLnZWVvHAF8BGsalQpOQ/KaY9hl8UIqleYqBcYa6sfX9vzbl66RVII7l30Hx2wKK6PhAs51NeGE1s3wg81fq80aC3vOhlwoAIK8w9gXKrctbg8bV0pf2uLUVkjFR63YgTsQbHJTux8fnRM99//x8quM3/g+qVVUsYBwmHbl6YEUxTyYsO+auYCLrsxBvPSa5JVXiTmDyz22NBOaDdNqjSVygyXB6nH7CZogze1IDOqbzNPy+Lu20bEAQKVXwU8kWIW22dWrNYVXDeCDYb8dkLZj9qPHIwDQeM4kgLqnEMfObvZJbgbbJ1SQ84gZ0RPFtgIic6KTel/8ToSVZRuBrz5p6Eb5J9kB1a6Xb/5uVenmtHA4y4L+he6Fuq07QRnfzTZw7Gi3hunwxPOzrQoE45MdSVD9gUCAwEAAQ==";
-	
 	public static void main(String[] args) throws ClassNotFoundException {
 		if (args == null || args.length != 3) {
 			System.err.println("Please provide [machine code] [cpu] [memory in gb] as parameter");
@@ -44,29 +39,21 @@ public class GenerateLicense {
 		
 		String machineCode = args[0];
 		int cpu = Integer.parseInt(args[1]);
-		long mem = Long.parseLong(args[2]);
+		long mem = Long.parseLong(args[2]) * 1024 * 1024 * 1024;
 		
 		LicenseBean lb = new LicenseBean();
 		lb.setCpuCount(cpu);
 		lb.setMemCount(mem);
 		lb.setMachineCode(machineCode);
 		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			ObjectOutputStream os = new ObjectOutputStream(baos);
 			os.writeObject(lb);
 			os.close();
-			String serializedLicense = new String(baos.toByteArray());
-			System.out.println("Machine Code is:" + machineCode);
-			System.out.println("Serialized Data:" + serializedLicense);
-			System.out.println("Data len:" + serializedLicense.getBytes("UTF-8").length);
-			String ll = encrypt(key, serializedLicense);
+			
+			String serializedLicense = Base64.encodeBase64String(baos.toByteArray());
 			System.out.println("License:" + encrypt(key, serializedLicense));
-			String dl = decrypt(ll);
-			System.out.println("Decrypt License:" + decrypt(ll));
-			ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(dl.getBytes()));
-			LicenseBean bean = (LicenseBean) ois.readObject();
-			System.out.println("bean:" + bean);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -74,7 +61,7 @@ public class GenerateLicense {
 	
 	private static final String encrypt(String key, String data) {
 		byte[] corekey = Base64.decodeBase64(key);
-
+		
 		PKCS8EncodedKeySpec pkspec = new PKCS8EncodedKeySpec(corekey);
 		
 		try {
@@ -91,23 +78,4 @@ public class GenerateLicense {
 			return null;
 		}
 	}
-	
-	private static final String decrypt(String data) {
-		byte[] corekey = Base64.decodeBase64(LICENSE_CORE_KEY);
-		byte[] rawData = Base64.decodeBase64(data);
-		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(corekey);
-		try {
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			Key publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
-
-			Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-			cipher.init(Cipher.DECRYPT_MODE, publicKey);
-			
-			return new String(cipher.doFinal(rawData), "UTF-8");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
 }
--- src/cn/lynx/emi/license/ViewLicense.java
+++ src/cn/lynx/emi/license/ViewLicense.java
@@ -4,23 +4,20 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Enumeration;
 
 import javax.crypto.Cipher;
 
 import org.apache.commons.codec.binary.Base64;
 
-public class LicenseTest {
+public class ViewLicense {
 	private static final String LICENSE_CORE_KEY = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAoc7HGhetqiAwyrVZxBskNusiz9TNPX0niaIi7C16DxnKguANpn1lDOk/U2T+gOJiLIt/zL3bvhjCXN0krD4lZUJZxC2RXSQG97622aFeYuYOKtkzmrNwlRK8RCeKlGbydF9V7O+LmKHEzlpttLx0pglw0x4ps4ALEc82wZErHhZ9m76m1ykoNOSY+Khz4OPhMVXKm0EYwitTktfSEsV/vIsXymbJCUprkN1Nw7ftjA3UyU9LvRhs1puczss8kp0WLE9gOB9dxzV+QrmLnZWVvHAF8BGsalQpOQ/KaY9hl8UIqleYqBcYa6sfX9vzbl66RVII7l30Hx2wKK6PhAs51NeGE1s3wg81fq80aC3vOhlwoAIK8w9gXKrctbg8bV0pf2uLUVkjFR63YgTsQbHJTux8fnRM99//x8quM3/g+qVVUsYBwmHbl6YEUxTyYsO+auYCLrsxBvPSa5JVXiTmDyz22NBOaDdNqjSVygyXB6nH7CZogze1IDOqbzNPy+Lu20bEAQKVXwU8kWIW22dWrNYVXDeCDYb8dkLZj9qPHIwDQeM4kgLqnEMfObvZJbgbbJ1SQ84gZ0RPFtgIic6KTel/8ToSVZRuBrz5p6Eb5J9kB1a6Xb/5uVenmtHA4y4L+he6Fuq07QRnfzTZw7Gi3hunwxPOzrQoE45MdSVD9gUCAwEAAQ==";
 	
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		if (args == null || args.length <= 0) {
-			System.out.println("Machine Code:" + _generateMachineCode());
+			System.err.println("Please provide the license string");
 			return;
 		}
 		
@@ -28,7 +25,7 @@ public class LicenseTest {
 			String license = args[0];
 			LicenseBean lb = retrieveLicense(license);
 			if (lb == null) {
-				System.err.println("License Bean can't be serialized.");
+				System.err.println("License Bean can't be deserialized.");
 				return;
 			}
 			
@@ -38,27 +35,10 @@ public class LicenseTest {
 		}
 	}
 	
-	private static final String _generateMachineCode() {
-		StringBuilder sb = new StringBuilder();
-		try {
-			Enumeration<NetworkInterface> enumsofnet = NetworkInterface.getNetworkInterfaces();
-			while (enumsofnet.hasMoreElements()) {
-				NetworkInterface nic = enumsofnet.nextElement();
-				byte[] mac = nic.getHardwareAddress();
-				if (mac != null) {
-					sb.append(Base64.encodeBase64String(mac));
-				}
-			}
-		} catch (SocketException e) {
-		}
-		return sb.toString();
-	}
-	
 	public static final LicenseBean retrieveLicense(String license) throws UnsupportedEncodingException {
-		String decryptedLicense = _decryptByPubKey(license);
-		System.out.println("Decrypted :" + decryptedLicense);
+		String decryptedLicense = _decrypt(license);
 		try {
-			ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(decryptedLicense.getBytes("UTF-8")));
+			ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(Base64.decodeBase64(decryptedLicense)));
 			LicenseBean bean = (LicenseBean) ois.readObject();
 			return bean;
 		} catch (IOException e) {
@@ -70,11 +50,7 @@ public class LicenseTest {
 		}
 	}
 
-	private static final String _decryptByPubKey(String data) throws UnsupportedEncodingException {
-		return _cryptoByKey(data, Cipher.DECRYPT_MODE);
-	}
-
-	private static final String _cryptoByKey(String data, int mode) throws UnsupportedEncodingException {
+	private static final String _decrypt(String data){
 		byte[] corekey = Base64.decodeBase64(LICENSE_CORE_KEY);
 		byte[] rawData = Base64.decodeBase64(data);
 		
@@ -84,7 +60,7 @@ public class LicenseTest {
 			Key publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
 
 			Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-			cipher.init(mode, publicKey);
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
 			
 			return new String(cipher.doFinal(rawData), "UTF-8");
 		} catch (Exception e) {
